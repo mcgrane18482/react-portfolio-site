00f50ea49da5aa4abb45330d4ab97965d0802a51
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-08-23 14:44:42 -0400

    started creating mobile menu bar

--- react-app/src/components/Header/index.jsx
+++ react-app/src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import './header.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,19 +7,29 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
 
+    const [menuIcon, setMenuIcon] = useState(false);
+
+    const toggleMenu = async e => {
+        setMenuIcon(!menuIcon);
+    };
+
     return (
         <header>
             <div className="header-content">
                 <div className="logo">E. MCGRANE</div>
 
 
-                <nav>
+                <nav className="navbar">
                     <NavLink to="/">HOME</NavLink>
                     <NavLink to="/about">ABOUT</NavLink>
                     <NavLink to="/portfolio">PORTFOLIO</NavLink>
                     <NavLink to="/contact">CONTACT</NavLink>
 
                 </nav>
+
+                <div id="mobile">
+                    <FontAwesomeIcon id="menu-icon" onClick={toggleMenu} icon={menuIcon ? faTimes : faBars} />
+                </div>
 
             </div>
         </header>
